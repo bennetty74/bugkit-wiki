8621a5b0ca023058cb1db4d98965351d7df0c12c
bennetty74 <bennetty74@gmail.com>
2022-01-19 14:02:50 +0800

    completed search engine selection

--- src/App.js
+++ src/App.js
@@ -11,7 +11,15 @@ function App() {
      * 搜索的key和setter
      */
     const [key, setKey] = useState('')
+    /**
+     * 搜索引擎
+     */
+    const [engine, setEngine] = useState('baidu')
 
+    /**
+     * 首页站点表
+     * @type {[{name: string, url: string}, {name: string, url: string}, {name: string, url: string}]}
+     */
     const siteList = [
         {name: 'Blog', url: 'https://blog.bugkit.cn/'},
         {name: 'Github', url: 'https://github.com/bennetty74'},
@@ -19,11 +27,21 @@ function App() {
     ]
 
     /**
+     * 搜索引擎Map
+     * @type {{baidu: string, google: string, bing: string}}
+     */
+    const engineMap = {
+        bing: 'https://cn.bing.com/search?q=',
+        google: 'https://www.google.com.hk/search?q=',
+        baidu: 'https://www.baidu.com/s?wd='
+    }
+
+    /**
      * 处理搜索事件
      */
     function handleSearch() {
         if (key) {
-            let url = 'https://www.google.com.hk/search?q=' + key;
+            let url = engineMap[engine] + key;
             window.open(url, '_blank');
         }
     }
@@ -37,7 +55,6 @@ function App() {
      * @param e
      */
     function handleEnter(e) {
-        console.log(e);
         if (e.key === 'Enter') {
             handleSearch();
         }
@@ -48,8 +65,15 @@ function App() {
      * @returns {boolean}
      */
     function isMobile() {
-        console.log(window.screen.width)
         return window.screen.width <= 600;
+    }
+
+    /**
+     * 处理搜索引擎变化事件
+     * @param e 事件
+     */
+    function handleEngineChange(e) {
+        setEngine(e.target.value)
     }
 
     /**
@@ -59,7 +83,6 @@ function App() {
      * @constructor
      */
     function SearchButton(props) {
-        console.log(props)
         if (props.isMobile) {
             return null;
         }
@@ -69,7 +92,7 @@ function App() {
     function SiteList() {
         const siteItems = siteList.map(item =>
             <div className="site-item" onClick={() => toTarget(item.url)} key={item.name}>
-                <div className="img">{item.name.substr(0,1).toUpperCase()}</div>
+                <div className="img">{item.name.substr(0, 1).toUpperCase()}</div>
                 <div className="name">{item.name}</div>
             </div>
         )
@@ -93,13 +116,13 @@ function App() {
                     </div>
                     <div className="search-wrapper">
                         <div className="search-input-group">
-                            <select className="select">
+                            <select className="select" onClick={(e) => handleEngineChange(e)}>
                                 <option value="baidu">Baidu</option>
                                 <option value="google">Google</option>
                                 <option value="bing">Bing</option>
                             </select>
                             <input value={key} className="search-input" placeholder="嗖嗖嗖..."
-                                                    onChange={e => setKey(e.target.value)} onKeyUp={event => handleEnter(event)}/>
+                                   onChange={e => setKey(e.target.value)} onKeyUp={event => handleEnter(event)}/>
                         </div>
                         <SearchButton isMobile={isMobile()}/>
                     </div>
